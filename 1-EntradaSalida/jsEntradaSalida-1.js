5835eed24c808a6021af475f1e6561aebc5a5c3e
FelipeBG1 <60651449+FelipeBG1@users.noreply.github.com>
2020-02-06 11:13:00 -0300

    Primer Ejercicio
    Un buen comienzo

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var primerPrecio; //si se quiere poner un número que sea despues el primer debe ser en letras no en número, si quiero poner un número que sea al final
+	var segundoPrecio; //lo mismo que con el anterior
+	var apellido="nadie";
+
+	apellido="perez";
+	alert(apellido  );
+	alert("tal cual");
+	//alert(primerpri); si la variable no existe no podemos hacer nada
+	// es un valor literal si esta entre comillas
+	//alert(" tal cual");//literal
+    //alert(apellido); indefinido
+    //alert("apellido"); va a aparecer la palabra apellido como la frase que aparece en la linea 10
+    //alert();sale vacio
 }
 
