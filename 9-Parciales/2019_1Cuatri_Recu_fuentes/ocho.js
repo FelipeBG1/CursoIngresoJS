f28effdbb2d86cda91eee09840a6c26ec3e8b825
FelipeBG1 <60651449+FelipeBG1@users.noreply.github.com>
2020-02-29 10:12:26 -0300

    Ejercicios 7 y 8 hechos en clase

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,94 @@
 function mostrar()
 {
+	var valor;
+	var color;
+	var contador;
+	var contadorRojos;
+	var contadorMayor5mil;
+	var contadorMenor5mil;
+	var contadorVehiculos;
+	var promedioVehiculos;
+	var masCaro;
+	var masCaroColor;
+	var sumaVehiculos;
+	var respuesta;
+
+	contador=0;
+	contadorRojos=0;
+	contadorVehiculos=0;
+	contadorMenor5mil=0;
+	contadorMayor5mil=0;
+	sumaVehiculos=0;
+	respuesta=true;
+
+
+	while(respuesta==true)
+	{
+		valor=prompt("Ingrese el valor del vehículo");
+		valor=parseInt(valor);
+
+		while(valor<0 || valor>10000)
+		{
+			valor=prompt("Error el valor debe estar entre 0 y 10000");
+			valor=parseInt(valor);	
+		}
+
+		color=prompt("Ingrese el color del vehículo");
+
+		while(color!="rojo" && color!="amarillo" && color!="verde")
+		{
+			color=prompt("Error solo rojo, verde o amarillo");
+		}
+	
+
+		
+		if (contador==0) 
+		{
+			masCaro=valor;
+			masCaroColor=color;
+		}
+		else
+		{
+			if (valor>masCaro) 
+			{
+				masCaro=valor;
+				masCaroColor=color;
+			}
+		}
+
+
+		if (color=="rojo" ) 
+		{
+			contadorRojos++;
+			
+		
+			if (valor>5000 ) 
+			{
+				contadorMayor5mil++;
+			}
+
+		}
+		else 
+		{
+			if ( valor<5000) 
+			{
+				contadorMenor5mil++;
+			}
+			
+		}
+	
+
+		contador++;
+		sumaVehiculos=sumaVehiculos+valor;
+		respuesta=confirm("Haga click en Aceptar para continuar")
+	}
+		
+	promedioVehiculos=sumaVehiculos/contador;
+
+	document.write("La cantidad de rojos es: "+contadorRojos+ "<br>");
+	document.write("La cantidad de rojos con precio mayor a 5000 es: "+contadorMayor5mil+ "<br>");
+	document.write("La cantidad de vehiculos con precio inferior a 5000 es: "+contadorMenor5mil+ "<br>");
+	document.write("El vehículo más caro es "+masCaro+ " y su color es "+masCaroColor+ "<br>");
+
 
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,79 @@
 function mostrar()
 {
+	var bolsaAlimentoKilos;
+	var sabor;
+	var contador;
+	var sumaKilos;
+	var masLiviano;
+	var contadorCarne;
+	var sumaKilosCarne;
+	var saborMasLiviano;
+	var promedioKilosTotales;
+	var promedioCarne;
+	
+
+	contador=0;
+	sumaKilos=0;
+	sumaKilosCarne=0;
+	contadorCarne=0;
+
+	while(contador<10)
+	{
+		bolsaAlimentoKilos=prompt("Ingrese la cantidad de kilos de la bolsa");
+		bolsaAlimentoKilos=parseInt(bolsaAlimentoKilos);
+
+		while(bolsaAlimentoKilos<0 || bolsaAlimentoKilos>500)
+		{
+			bolsaAlimentoKilos=prompt("Error solo valores de 0 a 500");
+			bolsaAlimentoKilos=parseInt(bolsaAlimentoKilos);
+		}
+	
+
+		sabor=prompt("Ingrese el sabor del alimento que se encuentra en la bolsa");
+
+		while(sabor!="carne" && sabor!="vegetales" && sabor!="pollo")
+		{
+			sabor=prompt("Error solo carne, vegetales o pollo como sabores");
+		}
+
+
+		if (contador==0) 
+		{
+			masLiviano=bolsaAlimentoKilos;
+			saborMasLiviano=sabor;
+		}
+		else
+		{
+			if (bolsaAlimentoKilos<masLiviano) 
+			{
+				masLiviano=bolsaAlimentoKilos;
+				saborMasLiviano=sabor;
+			}
+		}
+
+
+		if (sabor=="carne") 
+		{
+			contadorCarne++;
+			sumaKilosCarne=sumaKilosCarne+bolsaAlimentoKilos;
+		}  
+
+		contador++;
+		sumaKilos=sumaKilos+bolsaAlimentoKilos;
+		
+	}
+
+
+	promedioKilosTotales=sumaKilos/contador;
+	promedioCarne=sumaKilosCarne/contadorCarne;
+
+	alert("El promedio de los kilos totales es: "+promedioKilosTotales);
+	alert("El alimento mas liviano es "+masLiviano+ " y su sabor es "+saborMasLiviano);
+	alert("La cantidad de alimentos sabor carne es "+contadorCarne+ " y el promedio de kilos de sabor carne es "+promedioCarne);
+
+
+
+
+
 
 }
